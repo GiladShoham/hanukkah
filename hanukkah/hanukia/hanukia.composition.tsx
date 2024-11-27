65666cd6697611e5496ce499599f66cb4829fd74
Gilad Shoham <shoham.gilad@gmail.com>
2024-11-27 14:14:30 +0200

    add examples

--- hanukkah/hanukia/hanukia.composition.tsx
+++ hanukkah/hanukia/hanukia.composition.tsx
@@ -3,3 +3,35 @@ import { Hanukia } from './hanukia.js';
 export const BasicHanukia = () => {
   return <Hanukia></Hanukia>;
 };
+
+export const OneCandleOn = () => {
+  return <Hanukia on={1}></Hanukia>;
+};
+
+export const TwoCandlesOn = () => {
+  return <Hanukia on={2}></Hanukia>;
+};
+
+export const ThreeCandlesOn = () => {
+  return <Hanukia on={3}></Hanukia>;
+};
+
+export const FourCandlesOn = () => {
+  return <Hanukia on={4}></Hanukia>;
+};
+
+export const FiveCandlesOn = () => {
+  return <Hanukia on={5}></Hanukia>;
+};
+
+export const SixCandlesOn = () => {
+  return <Hanukia on={6}></Hanukia>;
+};
+
+export const SevenCandlesOn = () => {
+  return <Hanukia on={7}></Hanukia>;
+};
+
+export const EightCandlesOn = () => {
+  return <Hanukia on={8}></Hanukia>;
+};
